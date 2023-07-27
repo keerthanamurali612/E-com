c4d5dcf333d1561cb0518039e395fbc56edbea05
Keerthana <keerthanamurali099@gmail.com>
2023-07-27 07:52:39 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
+import { RouterModule ,Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+
+
+const routes:Routes =[
+  {path:'',component:HeaderComponent},
+  {path:'products' ,component:ProductsComponent},
+  {path:'cart',component:CartComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
